624e350f90f58c6fcca04575e7d6c8d045a4d48d
Chao <j00744382@students.jsums.edu>
2024-08-06 16:09:09 -0500

    Below is what have been done in this commit     1. Updated spinnerClicked() to be able to capture the user selection of a spinner inside meshAndSimuControlLayout;     2. Updated typeInsideTextInput() in order to be able to save a series of textinput data into a list and assign it to a single key in meshAndSimuControlLayout;     3. Binded all necessary methods to the each user input entry in meshAndSimuControlLayout;     4. All user input and update of entries inside meshAndSimuControlLayout will be saved in a dictonary variable entryDataDict in real time.

--- main.py
+++ main.py
@@ -58,16 +58,40 @@ class SolverChooserPopup(Popup):
 
 Builder.load_file("meshAndSimuControl.kv")
 class MeshAndSimuControlLayout(GridLayout):
+
+    """
+    * The method to save user selection as well as selection key to entryDataDict
+    * This will be bined to on_active method of a CheckBox object
+    """
     def checkboxSelected(self, theCheckbox, entryKey, selectedValue):
         entryDataDict[entryKey] = selectedValue
         print(entryDataDict)
         print(len(entryDataDict))
     
-    def spinnerClicked(self, selectedValue):
-        print("{}".format(selectedValue))
-
-    def typeInsideTextInput(self, key, value):
-        entryDataDict[key] = value
+    """
+    * The method to save user selection as well as selection key of a spinner to entryDataDict
+    * this will be bined to on_text method of a Spinner object
+    """
+    def spinnerClicked(self, entryKey, selectedValue):
+        entryDataDict[entryKey] = selectedValue
+        print(entryDataDict)
+    
+    """
+    * The method to save user input inside TextInput box to entryDataDict
+    * This will be bined to on_text method of a TextInput object
+    * Kwarg seqLen: if it is larger than 0, it means a series of textInputs will be wrapped into a list to assgin to a single key in the entryDataDict
+    * Kwarg idxOfCurrentTextIpt represents the index for the current editing textInput inside the list of the series fo textInputs
+    """
+    def typeInsideTextInput(self, key, value, *, seqLen = 0, idxOfCurrentTextIpt = -1):
+        if seqLen > 0:
+            if key in entryDataDict:
+                listOfValues = entryDataDict[key]
+                listOfValues[idxOfCurrentTextIpt] = value
+            else:
+                entryDataDict[key] = ["" for i in range(seqLen)]
+                entryDataDict[key][idxOfCurrentTextIpt] = value
+        else:
+            entryDataDict[key] = value
         print(entryDataDict)
 
 
