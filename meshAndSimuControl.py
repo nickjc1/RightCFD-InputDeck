492fb461ffb272575b85456961d600d89464bdc8
Chao <35981611+nickjc1@users.noreply.github.com>
2024-10-02 22:57:35 -0500

    Below shows what has been updated in this commit: (#8)
    Fixed the bug:
        - Data of steady_monitor from in file could be either a single string or a list, in these case current id catalog can not handle these problem.
        - So we add a new id catalog containing keyword "2version" which will be used to handle above issue.
    
    New feature:
        + Added 3 new solvers into the solverChooserPopup(Popup) widget.
    
    File update:
        inFileManagement.py:
            1. Modified showDataToInFileDetailsWindow(self, *, theWindow) to handle 2version id.
    
        meshAndSimuControl.kv:
            1. Changed id under steady_monitor into steady_monitor-2version_text_#.
    
        meshAndSimuControl.py:
            1. Modified typeInsideTextInput(self, key, value, *, seqLen = 1, idxOfCurrentTextIpt = -1) to be able to convert steady_monitor entry into a list if it is not when ready from a in file.
    
        solverChooserPopup.kv:
            1. Added new solver buttons.
    
    Co-authored-by: Chao <j00744382@students.jsums.edu>

--- meshAndSimuControl.py
+++ meshAndSimuControl.py
@@ -29,14 +29,23 @@ class MeshAndSimuControlLayout(GridLayout):
     * Kwarg seqLen: if it is larger than 0, it means a series of textInputs will be wrapped into a list to assgin to a single key in the entryDataDict
     * Kwarg idxOfCurrentTextIpt represents the index for the current editing textInput inside the list of the series fo textInputs
     """
-    def typeInsideTextInput(self, key, value, *, seqLen = 0, idxOfCurrentTextIpt = -1):
-        if seqLen > 0:
+    def typeInsideTextInput(self, key, value, *, seqLen = 1, idxOfCurrentTextIpt = -1):
+        if seqLen > 1:
             if key in entryDataDict:
-                listOfValues = entryDataDict[key]
-                listOfValues[idxOfCurrentTextIpt] = value
+                """
+                * For the id that contains 2version, whose value can be either a LIST or a single STRING,
+                * when the entry of this id in entryDataDict trys to show on inFileDetails window,
+                * first check if this entry is a list:
+                *     If it is, re-update this list[indxOfCurrentTextIpt] while initializing the value of this id in inFileDetails window;
+                *     If it is not, convert this entry into a list with a length of arg:seqLen and the first item is the arg:value.   
+                """
+                if type(entryDataDict[key]) == list:
+                    entryDataDict[key][idxOfCurrentTextIpt] = value
+                else:
+                    entryDataDict[key] = [value if i == 0 else "" for i in range(seqLen) ]
             else:
                 entryDataDict[key] = ["" for i in range(seqLen)]
                 entryDataDict[key][idxOfCurrentTextIpt] = value
         else:
             entryDataDict[key] = value
-        # print(entryDataDict)
+        print(entryDataDict["steady_monitor"])
