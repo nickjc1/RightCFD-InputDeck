89cb60b884556dde11ee1c59c82e036297b37ab2
Chao <35981611+nickjc1@users.noreply.github.com>
2024-12-18 16:13:05 -0600

    Boundary Condition Layout and its functionality Done (#10)
    * Below shows what has been updated in this commit:
    
    Fixed the bug: No
    
    New feature:
        + Added a new tab: complete the feature of add and delete button in boundaryCondition layout.
    
    * Below shows what has been updated in this commit:
    
    Bug fixed: Yes
        + Change simuControl Layout: fixed_dt entry to be textInput in order to fit space charge in.txt file
    
    New feature: Yes
        + AddOneMoreEntry() is able to extend the list as the value for the key: bdry_name and bc in entryDataDict
        + Defined BoundaryConditionLayout(BoxLayout):typeInsideTextInput() which is albe to capture user typed data into entryDataDict
        + Bound typeInsideTextInput to all the textInputs inside BoundaryConditionLayout
        + Completed deleteOneBdryEntry() so that it can not only remove last line of entries from the BoundaryConditonLayout but also remove the data from entryDataDict
    
    
    * Below shows what has been updated in this commit:
    
        Fixed the bug: No
    
        New feature: Yes
            + Completed the feature that can save/export bdry_name and bc data to the file.
        On branch bdry-feature
        Changes to be committed:
            modified:   inFileManagement/inFileManagement.py
    
    
    * Below shows what has been updated in this commit:
    
    Fixed the bug: Yes
        + Fixed the bug that when import data from a file, the BoundaryConditionLayout:on_kv_post() and BoundaryConditionLayout:entryiesInitialize() will wiped out the data that has been read to the entryDataDict
    New feature: Yes
        + Completed the feature that App can read and display the data for the boundary condition to the boundary condition panel.
    
    Changes to be committed:
        modified:   boundaryCondition/boundaryCondition.kv
        modified:   boundaryCondition/boundaryCondition.py
        modified:   inFileManagement/inFileManagement.py
    
    ---------
    
    Co-authored-by: Chao <j00744382@students.jsums.edu>

--- boundaryCondition/boundaryCondition.py
+++ boundaryCondition/boundaryCondition.py
@@ -10,90 +10,219 @@ from customizedComponents.customizedComponents import *
 Builder.load_file("boundaryCondition/boundaryCondition.kv")
 class BoundaryConditionLayout(BoxLayout):
 
-    # This method is called after the kv properties have been set
+    """
+    This method is called after the kv properties have been set
+    """
     def on_kv_post(self, base_widget):
-        self.currentNumOfEntry = 1 
-        # self.entriesInitialize(numOfEntry=self.currentNumOfEntry)
+        self.currentNumOfEntry = 1
+
+        if "nbc" not in entryDataDict: # in case if we import data from .txt file, so that below lines won't wipe out the data that has been read.
+            entryDataDict["nbc"] = 1
+            entryDataDict["bdry_name"] = ["1", "", ""]
+            entryDataDict["bc"] = ["1", "", "", "", ""]
+
+        print(entryDataDict)
 
         return super().on_kv_post(base_widget)
     
 
     """
-    bdry_nameEtriesInitialize() is used to initialize the bdry_name entries block inside BoundaryConditionLayout. 
+    etriesInitialize() is used to initialize the bdry entries block inside BoundaryConditionLayout. 
     """
     def entriesInitialize(self, *, numOfEntry):
-        
         for i in range(numOfEntry):
-            bdry_nameLabel = BlackLabel()
-            bdry_nameLabel.text = "bdry_name"
+            self.addOneMoreEntry(isNewEntry=False)
+            self.currentNumOfEntry == 1
 
-            seqLabel = BlackLabel()
-            seqLabel.halign = "right"
-            seqLabel.text = "{}".format(i+1)
-            self.ids["bdry_nameSeqLabel{}".format(i+1)] = seqLabel
+    
+    """
+    """
+    def addOneMoreEntry(self, *, isNewEntry):
+        self.addOneMoreBdryNameEntry()
+        self.addOneMoreBdryDetailEntry() 
 
-            bdnumTextInput = TextInput()
-            bdnumTextInput.multiline = False
-            bdnumTextInput.text = ""
-            self.ids["bdry_nameBdnumTextInput{}".format(i+1)] = bdnumTextInput
+        self.currentNumOfEntry += 1
 
-            nameTextInput =  TextInput()
-            nameTextInput.multiline = False
-            bdnumTextInput.text = ""
-            self.ids["nameTextInput{}".format(i+1)] = nameTextInput
+        self.ids["nbc-"].text = "{}".format(self.currentNumOfEntry)
+        entryDataDict["nbc"] = self.currentNumOfEntry
 
+        newBdryNameEntrySubList = ["{}".format(self.currentNumOfEntry), "", ""]
+        newBdryDetailEntrySubList = ["{}".format(self.currentNumOfEntry), "", "", "", ""]
 
-            # create a pointer that points to the GridLayout object inside self.
-            theLayout = self.ids["bdry_nameGridLayout"]
-
-            theLayout.add_widget(bdry_nameLabel)
-            theLayout.add_widget(seqLabel)
-            theLayout.add_widget(bdnumTextInput)
-            theLayout.add_widget(nameTextInput)
-
-        # print(self.ids)
-
-    def addOneMoreEntry(self):
-        bdry_nameLabel = BlackLabel()
-        bdry_nameLabel.text = "bdry_name"
-
-        seqLabel = BlackLabel()
-        seqLabel.halign = "right"
-        seqLabel.text = "{}".format(self.currentNumOfEntry+1)
-        self.ids["bdry_nameSeqLabel{}".format(self.currentNumOfEntry+1)] = seqLabel
-
-        bdnumTextInput = TextInput()
-        bdnumTextInput.multiline = False
-        bdnumTextInput.text = ""
-        self.ids["bdry_nameBdnumTextInput{}".format(self.currentNumOfEntry+1)] = bdnumTextInput
-
-        nameTextInput =  TextInput()
-        nameTextInput.multiline = False
-        bdnumTextInput.text = ""
-        self.ids["nameTextInput{}".format(self.currentNumOfEntry+1)] = nameTextInput
-
-
+        if isNewEntry: # if It is to create a new empty entry for user to type in, set to True; If it is to set up certain number of entries for import data, set to False
+            entryDataDict["bdry_name"].extend(newBdryNameEntrySubList)
+            entryDataDict["bc"].extend(newBdryDetailEntrySubList)
+            
+        # print(self.ids.keys())
+        # print(entryDataDict)
+    
+    
+    """
+    """
+    def addOneMoreBdryNameEntry(self):
         # create a pointer that points to the GridLayout object inside self.
-        theLayout = self.ids["bdry_nameGridLayout"]
-
-        theLayout.add_widget(bdry_nameLabel)
-        theLayout.add_widget(seqLabel)
-        theLayout.add_widget(bdnumTextInput)
-        theLayout.add_widget(nameTextInput) 
-
-        print(theLayout.height)
         # self.height = self.minimum_height
         # print(self.height)
         # self.ids["bdry_nameGridLayout"].height = self.ids["bdry_nameGridLayout"].minimum_height
         # self.parent.do_layout()
 
         # currentScroll_y = self.parent.scroll_y
+        bdry_nameLabel = BlackLabel()
+        bdry_nameLabel.text = "bdry_name"
+        self.ids["bdry_name-{}1".format(self.currentNumOfEntry+1)] = bdry_nameLabel
+
+        seqLabel = BlackLabel()
+        seqLabel.halign = "right"
+        seqLabel.text = "{}".format(self.currentNumOfEntry+1)
+        self.ids["bdseq-{}2".format(self.currentNumOfEntry+1)] = seqLabel
+
+        bdnumTextInput = TextInput()
+        bdnumTextInput.multiline = False
+        bdnumTextInput.text = ""
+        bdnumTextInput.id = "bdnum-text-{}3".format(self.currentNumOfEntry+1) 
+        self.ids[bdnumTextInput.id] = bdnumTextInput
+        bdnumTextInput.bind(text=self.typeInsideTextInput)
+
+        nameTextInput =  TextInput()
+        nameTextInput.multiline = False
+        nameTextInput.text = ""
+        nameTextInput.id = "bdname-text-{}4".format(self.currentNumOfEntry+1)
+        self.ids[nameTextInput.id] = nameTextInput
+        nameTextInput.bind(text=self.typeInsideTextInput)
 
 
-        
-        self.currentNumOfEntry += 1
+        theLayout = self.ids["bdry_nameGridLayout"]
+        theLayout.add_widget(bdry_nameLabel)
+        theLayout.add_widget(seqLabel)
+        theLayout.add_widget(bdnumTextInput)
+        theLayout.add_widget(nameTextInput) 
+
+        theLayout = None
+
+
+    """
+    """
+    def addOneMoreBdryDetailEntry(self):
+        bc_label = BlackLabel()
+        bc_label.text = "bc"
+        self.ids["bc-{}1".format(self.currentNumOfEntry + 1)] = bc_label
+
+        seqLabel = BlackLabel()
+        seqLabel.text = "{}".format(self.currentNumOfEntry + 1)
+        seqLabel.halign = "right"
+        self.ids["bc_seq-{}2".format(self.currentNumOfEntry + 1)] = seqLabel
+
+        bc_sc1 = TextInput()
+        bc_sc1.multiline = False
+        bc_sc1.text = ""
+        bc_sc1.id = "bc_sc-text-{}3".format(self.currentNumOfEntry + 1)
+        self.ids[bc_sc1.id] = bc_sc1
+        bc_sc1.bind(text=self.typeInsideTextInput)
+
+        bc_sc2 = TextInput()
+        bc_sc2.multiline = False
+        bc_sc2.text = ""
+        bc_sc2.id = "bc_sc-text-{}4".format(self.currentNumOfEntry + 1)
+        self.ids[bc_sc2.id] = bc_sc2
+        bc_sc2.bind(text=self.typeInsideTextInput)
+
+        bc_p1 = TextInput()
+        bc_p1.multiline = False
+        bc_p1.text = ""
+        bc_p1.id = "bc_p-text-{}5".format(self.currentNumOfEntry + 1)
+        self.ids[bc_p1.id] = bc_p1
+        bc_p1.bind(text=self.typeInsideTextInput)
+
+        bc_p2 = TextInput()
+        bc_p2.multiline = False
+        bc_p2.text = ""
+        bc_p2.id = "bc_p-text-{}6".format(self.currentNumOfEntry + 1)
+        self.ids[bc_p2.id] = bc_p2
+        bc_p2.bind(text=self.typeInsideTextInput)
+
+        theLayout = self.ids["bcGridLayout"]
+        theLayout.add_widget(bc_label)
+        theLayout.add_widget(seqLabel)
+        theLayout.add_widget(bc_sc1)
+        theLayout.add_widget(bc_sc2)
+        theLayout.add_widget(bc_p1)
+        theLayout.add_widget(bc_p2)
+
+        theLayout = None
+
+
+    """
+    """    
+    def deleteOneBdryEntry(self): 
+        if self.currentNumOfEntry > 1 :
+            bdry_nameGridLayout = self.ids["bdry_nameGridLayout"]
+            bcGridLayout = self.ids["bcGridLayout"]
             
-    
+             
+            idToBeDeleted = []# temporary list that is used to store the ids that are goint to be removed.
+
+            # Get suffixes of the ids of the last line which is going to be deleted.
+            suffixes = ["{}{}".format(self.currentNumOfEntry, i) for i in range(1, 7)]
+            # print(suffixes)
+
+            # remove the widgets of last line of entries from the layout
+            for (id_key, widget) in self.ids.items():
+                if any(id_key.endswith("-{}".format(i)) for i in suffixes):
+                    idToBeDeleted.append(id_key)
+                    if widget in bdry_nameGridLayout.children:
+                        bdry_nameGridLayout.remove_widget(widget)
+                    else:
+                        bcGridLayout.remove_widget(widget)
+            
+            # remove the ids relating to the removed widgets from self.ids dictionary
+            for id_key in idToBeDeleted:
+                if id_key in self.ids:
+                    self.ids.pop(id_key)
+
+            # remove last line of entries from the entryDataDict     
+            for i in range(0, 3):
+                entryDataDict["bdry_name"].pop(-1)
+            for i in range(0, 5):
+                entryDataDict["bc"].pop(-1)
+
+            # print(entryDataDict)
+            # print(self.ids.keys())
+            
+            self.currentNumOfEntry -= 1
+            self.ids["nbc-"].text = "{}".format(self.currentNumOfEntry)
+
+            bdry_nameGridLayout = None
+            bcGridLayout = None
+
+
+
+
+    def typeInsideTextInput(self, instance, value, *, idFromKv=""):
+
+        # if the id is assigned in the .kv file, the instance.id won't exist. Thus the id needs to be pass here directly through idFromKv parameter
+        if(idFromKv == ""):
+            id = instance.id
+        else:
+            id = idFromKv
+        
+        # print(id)
+
+        idInfos = id.split("-")
+        idNum = int(idInfos[-1])
+        if (idInfos[0].startswith("bd")):
+            index = (idNum//10 - 1)*4 + (idNum%10) - (idNum//10)- 1
+            # print("index is {}".format(index))
+            entryDataDict["bdry_name"][index] = value
+        elif (idInfos[0].startswith("bc")):
+            index = (idNum//10 - 1)*6 + (idNum%10) - (idNum//10)- 1
+            entryDataDict["bc"][index] = value
+
+        print(entryDataDict)
+
+
+
+
+
 
 
 
--- inFileManagement/inFileManagement.py
+++ inFileManagement/inFileManagement.py
@@ -95,7 +95,7 @@ class InFileManagementPopupWindowLayout(Popup):
             self.dismiss()
 
     """
-    * Cross compair keys inside entryDataDict and ids from meshAndSimuControlLayoutPannel.ids
+    * Cross compair keys inside entryDataDict and ids from eachLayout.ids
     * grab approrate data and put them into widgets of inFileDetailsWindow screen.
     """    
     def showDataToInFileDetailsWindow(self, *, theWindow):
@@ -148,6 +148,46 @@ class InFileManagementPopupWindowLayout(Popup):
                             ids[key + "-"].text = entryDataDict[key]
                             break
 
+        meshControlLayoutTab = None
+        simuControlLayoutTab = None
+        laSolverLayoutTab = None
+        
+        # special keys:
+
+        # import data of boundary condition
+        boundaryConditionLayoutTab = theWindow.ids["BoundaryConditionLayout"]
+        numOfRows = int(entryDataDict["nbc"])
+        boundaryConditionLayoutTab.entriesInitialize(numOfEntry = (numOfRows - 1)) # - 1 because by default when this layout posted, there has been 1 row there already.
+        self.__showDataOfSpecialKeys(boundaryConditionLayoutTab, "bdry_name", numOfRows, "bd")
+        self.__showDataOfSpecialKeys(boundaryConditionLayoutTab, "bc", numOfRows, "bc")
+
+        boundaryConditionLayoutTab = None
+
+
+        
+
+    """
+    This Private method is used for import data, which is stored as a list in entryDataDict["key"] and is used to generate a table in the layout. 
+    """
+    def __showDataOfSpecialKeys(self, layoutToBeShown, specialKey, numOfRows, idStartsWith):
+        # numOfRows = int(entryDataDict["nbc"])
+        print()
+        numOfCol = len(entryDataDict[specialKey])//numOfRows
+        print(numOfCol)
+        index = 0
+        for data in entryDataDict[specialKey]:
+            pos = "{}{}".format(index//numOfCol + 1, index%numOfCol + 2)
+            for id in layoutToBeShown.ids:
+                idDetails = id.split("-")
+                if idDetails[-1] == pos and idDetails[0].startswith(idStartsWith):
+                    layoutToBeShown.ids[id].text = data
+                    break
+            index += 1
+
+
+
+        
+
 
 class InFileChooser(FileChooserListView):
     pass
@@ -155,7 +195,7 @@ class InFileChooser(FileChooserListView):
 
 class FileManagement:
 
-    TestEntryData = {}
+    # TestEntryData = {}
 
     @classmethod
     def readFile(cls, *, filePathAndName):
@@ -187,13 +227,44 @@ class FileManagement:
     def writeToFile(cls, *, filePathAndName):
         with open(filePathAndName, "w") as file:
             for key in entryDataDict:
-                if isinstance(entryDataDict[key], list):
+                if (key == "bdry_name" or key == "bc"):
+                    cls.__specialWrite(file, key)
+                elif isinstance(entryDataDict[key], list):
                     str = ""
                     for item in entryDataDict[key]:
                         str  = str + item + " "
                     file.write("{} {}\n".format(key, str))
                 else:
                     file.write("{} {}\n".format(key, entryDataDict[key]))
+    
+    @classmethod
+    def __specialWrite(cls, file, specialKey):
+
+        specialKeyPairs = {"bdry_name": "nbc", "bc": "nbc"}
+
+        """
+        numbers of rows and cols are based on the value(list) of the key.
+        eg: bdry_name: [1, 1, a, 2, 2, b, 3, 3, c, 4, 4, d]
+            numOfRows: 4
+            numOfCols: 3
+        """
+        numOfRows = entryDataDict[specialKeyPairs[specialKey]]
+        numOfCols = len(entryDataDict[specialKey])//numOfRows
+
+        # print("numOfRows: {}, numOfCols: {}".format(numOfRows, numOfCols))
+
+        for i in range(0, numOfRows):
+            file.write("{} ".format(specialKey))
+            str = ""
+            for j in range(0, numOfCols):
+                index = i*numOfCols + j
+                # print("i: {}, j: {}, index: {}".format(i, j, index))
+                str += "{} ".format(entryDataDict[specialKey][index])
+            file.write("{}\n".format(str))
+
+
+            
+
 
 
         
